f803faef1a4fab0f5f7522b56101ff2bd7c61ad3
Sergey <sergey@example.com>
2024-05-23 13:39:13 +0300

    prettifier + some refactorings

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import TaskList from "./TaskList";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import TasksStorage from "./TasksStorage";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
-
-  const tasksStorage = new TasksStorage();
-
   return (
     <>
-      <TaskList title="Todo list" storage={tasksStorage} />
+      <TaskList title="Todo list" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,103 +1,142 @@
-import { useState, useEffect } from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import { useState, useEffect } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 import TaskOperationDelete from "./TaskOperationDelete";
 import TaskOperationDone from "./TaskOperationDone";
 import TaskTitleEditable from "./TaskTitleEditable";
 import TaskOperationCreate from "./TaskOperationCreate";
-import TasksStorage from "./TasksStorage";
+import axios from "axios";
 
 type Task = {
-    id: number; 
-    title: string;
-    isDone: boolean;
-}
+  id: number;
+  title: string;
+  isDone: boolean;
+};
 
 interface Props {
-    title: string,
-    storage: TasksStorage, // an interface would be better, but i'm lazy
+  title: string;
 }
 
 function TaskList(props: Props) {
+  const [data, setData] = useState<Task[]>([]);
 
-    const [data, setData] = useState<Task[]>([]);
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
-    useEffect(() => {
-        const itemsData = props.storage.getItems();
-        setData(itemsData);
-        props.storage.setItems(itemsData);
-    }, []);
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get(import.meta.env.VITE_API_URL_TASKS);
+      const itemsData = response.data;
+      setData(itemsData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    const removeTask = (taskId: number) => {
-        let updatedData = data.filter(item => item.id !== taskId)
+  const removeTask = (taskId: number) => {
+    axios
+      .delete(import.meta.env.VITE_API_URL_TASKS + "/" + taskId)
+      .then(function () {
+        let updatedData = data.filter((item) => item.id !== taskId);
         setData(updatedData);
-        props.storage.setItems(updatedData);
-    };
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    const changeTaskState = (taskId: number, isDone: boolean) => {
-        const updatedData = data.map(item => item)
-        const item = updatedData.find((element) => element.id == taskId);
-        if (item !== undefined) {
-            item.isDone = isDone;
-            setData(updatedData);
-            props.storage.setItems(updatedData);
-        }
-    };
+  const getTaskByIdFromState = (taskId: number) => {
+    return data.find((element) => element.id == taskId);
+  };
 
-    const saveTask = (taskId: number, title: string) => {
-        const updatedData = data.map(item => item)
-        const item = updatedData.find((element) => element.id == taskId);
-        if (item !== undefined) {
-            item.title = title;
-            setData(updatedData);
-            props.storage.setItems(updatedData);
-        }
-    };
+  const changeTaskState = (taskId: number, isDone: boolean) => {
+    const task = getTaskByIdFromState(taskId);
+    if (task) {
+      task.isDone = isDone;
+      updateTask(task);
+    }
+  };
 
-    const createTask = (title: string) => {
-        const updatedData = data.map(item => item)
-        const maxId = updatedData.reduce(
-          (accumulator, item) => Math.max(accumulator, item.id),
-          0,
-        );
+  const saveTask = (taskId: number, title: string) => {
+    const task = getTaskByIdFromState(taskId);
+    if (task) {
+      task.title = title;
+      updateTask(task);
+    }
+  };
 
-        const task = {id: maxId + 1, title: title, isDone: false};
-        updatedData.push(task)
+  const updateTask = (task: Task) => {
+    axios
+      .put(import.meta.env.VITE_API_URL_TASKS + "/" + task.id, task)
+      .then(function () {
+        setData([...data]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const createTask = async (title: string) => {
+    const updatedData = data.map((item) => item);
+    const maxId = updatedData.reduce(
+      (accumulator, item) => Math.max(accumulator, item.id),
+      0
+    );
+
+    const task = { id: maxId + 1, title: title, isDone: false };
+
+    axios
+      .post(import.meta.env.VITE_API_URL_TASKS, task)
+      .then(function (response) {
+        const createdTask = response.data[0];
+        updatedData.push(createdTask);
         setData(updatedData);
-        props.storage.setItems(updatedData);
-    };
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    return (
+  return (
     <>
-        <Grid container spacing={2}>
+      <Grid container spacing={2}>
         <Grid item xs={8}>
-            <Typography variant="h4" sx={{ ml: 1}} gutterBottom>
+          <Typography variant="h4" sx={{ ml: 1 }} gutterBottom>
             {props.title}
-            </Typography>
+          </Typography>
         </Grid>
         <Grid item xs={4}>
-            <TaskOperationCreate onAdd={createTask} />
+          <TaskOperationCreate onAdd={createTask} />
         </Grid>
-        </Grid>
+      </Grid>
 
-        {data.length ? (
-
-        <List sx={{ bgcolor: 'background.paper' }}>
-        {data.map(item => (
+      {data.length ? (
+        <List sx={{ bgcolor: "background.paper" }}>
+          {data.map((item) => (
             <ListItem key={item.id}>
-                <TaskOperationDone onChangeState={changeTaskState} taskId={item.id} isDone={item.isDone} />
-                <TaskTitleEditable onSave={saveTask} taskId={item.id} title={item.title} isDone={item.isDone} />
-                <TaskOperationDelete onRemove={removeTask} taskId={item.id} />
+              <TaskOperationDone
+                onChangeState={changeTaskState}
+                taskId={item.id}
+                isDone={item.isDone}
+              />
+              <TaskTitleEditable
+                onSave={saveTask}
+                taskId={item.id}
+                title={item.title}
+                isDone={item.isDone}
+              />
+              <TaskOperationDelete onRemove={removeTask} taskId={item.id} />
             </ListItem>
-        ))}
+          ))}
         </List>
-        ) : (
-            <p>No items.</p>
-        )}
+      ) : (
+        <p>No items.</p>
+      )}
     </>
-    );
+  );
 }
 
 export default TaskList;
